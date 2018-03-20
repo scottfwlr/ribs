a3e235081c9478c9de3ae21ae119059d1beab203
scottfwlr <this.specific.scott@gmail.com>
2018-03-20 18:07:31 -0400

    implements has_one_through

--- lib/spine/associatable.rb
+++ lib/spine/associatable.rb
@@ -26,6 +26,37 @@ module Associatable
     end
   end
 
+  def has_one_through(name, through_name, source_name)
+    through = assoc_options[through_name]
+    define_method(name) do
+      source = through.model_class.assoc_options[source_name]
+
+      # source_table = source_options.table_name
+      # through_table = through_options.table_name
+      # source_foreign_key = source_options.foreign_key
+      # through_foreign_key = through_options.foreign_key
+      # source_primary_id = source_options.primary_key
+      # through_primary_id = through_options.primary_key
+
+      SQL = """
+        SELECT
+          #{source.table_name}.*
+        FROM
+          #{through.table_name}
+        JOIN
+          #{source.table_name} ON #{through.table_name}.#{source.foreign_key} = #{source.table_name}.#{source.primary_key}
+        WHERE
+          #{through.table_name}.#{through.primary_id} = ?
+        LIMIT
+          1
+      """
+
+      results = DB.execute(SQL, send(through.foreign_key))
+      
+      source.model_class.parse(results).first
+    end
+  end
+
   def assoc_options
     @assoc_options ||= {}
   end
