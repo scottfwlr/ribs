8b64377ada37b6bc8bdd1a6d90d31450465b1b26
scottfwlr <this.specific.scott@gmail.com>
2018-05-20 14:08:53 -0400

    clean slate

--- app.rb
+++ app.rb
@@ -15,19 +15,34 @@ require_relative 'controllers/root_controller'
 require_relative 'controllers/papers_controller'
 require_relative 'controllers/scientists_controller'
 require_relative 'controllers/institutes_controller'
-
+ 
 # Define our routes
 
 router = Router.new
 router.draw do
   get '/papers/:id', PapersController, :show
   get '/papers', PapersController, :index
+  get '/papers/new', PapersController, :new
+  post '/papers', PapersController, :create
+  get '/papers/:id/edit', PapersController, :edit
+  put '/papers/:id', PapersController, :update
+  delete '/papers/:id', PapersController, :destroy
 
   get '/scientists/:id', ScientistsController, :show
   get '/scientists', ScientistsController, :index
+  get '/scientists/new', ScientistsController, :new
+  post '/scientists', ScientistsController, :create
+  get '/scientists/:id/edit', ScientistsController, :edit
+  put '/scientists/:id', ScientistsController, :update
+  delete '/scientists/:id', ScientistsController, :destroy
 
   get '/institutes/:id', InstitutesController, :show
   get '/institutes', InstitutesController, :index
+  get '/institutes/new', InstitutesController, :new
+  post '/institutes', InstitutesController, :create
+  get '/institutes/:id/edit', InstitutesController, :edit
+  put '/institutes/:id', InstitutesController, :update
+  delete '/institutes/:id', InstitutesController, :destroy
 
   get '/', RootController, :root
 end
